620bfdf8e310a64aecbbb9956450668e378f2872
64070145Rosjana <73657654+64070145Rosjana@users.noreply.github.com>
2020-11-14 21:57:57 +0700

    Add files via upload

--- delete.php
+++ delete.php
@@ -1,5 +1,5 @@
 <?php
-
+$delete_id=$_GET['delete_id']
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'itf45.mysql.database.azure.com', 'rosjana@itf45', 'Pang0990062471', 'ITFLab', 3306);
 if (mysqli_connect_errno($conn))
@@ -7,17 +7,15 @@ if (mysqli_connect_errno($conn))
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
-$name = $_POST['name'];
-$comment = $_POST['comment'];
-$link = $_POST['link'];
+$sql="DELETE FROM guesbook WHERE Id ='$delete_id'"
 
 $sql = "DELETE FROM guestbook WHERE name";
-
-if (mysqli_query($conn, $sql)) {
-  echo "Record deleted successfully";
+$result = mysql_query($sql);
+if($result){
+    echo"<script type='text/javascript'>alert('ลบข้อมูลสำเร็จ')</script>";//javascript แจ้ง alert ข้อความ
+    echo "<meta http-equiv ='refresh'content='0;URL=show.php'>";// คำสั่งให้ refreshหน้าไปหน้าที่เราต้องการ
 } else {
-  echo "Error deleting record: " . mysqli_error($conn);
-}
+    echo"<script type='text/javascript'>alert('ลบข้อมูลไม่สำเร็จ');window.history.go(-1);</script>";//javascript แจ้ง alert ข้อความ และคำสั่งให้ refresh หน้าเดิมถ้าลบข้อความไม่สำเร็จ
+}    
 
-mysqli_close($conn);
 ?>
