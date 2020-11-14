dbcc9a02960d33a282abeb9c46ad380b5fcf62c4
64070145Rosjana <73657654+64070145Rosjana@users.noreply.github.com>
2020-11-14 21:32:52 +0700

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,23 @@
+<?php
+
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'itf45.mysql.database.azure.com', 'rosjana@itf45', 'Pang0990062471', 'ITFLab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+$name = $_POST['name'];
+$comment = $_POST['comment'];
+$link = $_POST['link'];
+
+$sql = "DELETE FROM guestbook (Name , Comment , Link) WHERE id=('$name', '$comment', '$link')";
+
+if (mysqli_query($conn, $sql)) {
+  echo "Record deleted successfully";
+} else {
+  echo "Error deleting record: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
