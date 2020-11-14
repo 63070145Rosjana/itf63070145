5104c1f32623056bef386fd99bac0f012abc1ee9
64070145Rosjana <73657654+64070145Rosjana@users.noreply.github.com>
2020-11-14 22:17:31 +0700

    Add files via upload

--- delete.php
+++ delete.php
@@ -1,5 +1,4 @@
 <?php
-$delete_id=$_GET['delete_id']
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'itf45.mysql.database.azure.com', 'rosjana@itf45', 'Pang0990062471', 'ITFLab', 3306);
 if (mysqli_connect_errno($conn))
@@ -7,15 +6,14 @@ if (mysqli_connect_errno($conn))
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
-$sql="DELETE FROM guesbook WHERE Id ='$delete_id'"
+// sql to delete a record
+$sql = "DELETE FROM MyGuests WHERE id='$delete_id'";
 
-$sql = "DELETE FROM guestbook WHERE name";
-$result = mysql_query($sql);
-if($result){
-    echo"<script type='text/javascript'>alert('ลบข้อมูลสำเร็จ')</script>";//javascript แจ้ง alert ข้อความ
-    echo "<meta http-equiv ='refresh'content='0;URL=show.php'>";// คำสั่งให้ refreshหน้าไปหน้าที่เราต้องการ
+if ($conn->query($sql) === TRUE) {
+  echo "Record deleted successfully";
 } else {
-    echo"<script type='text/javascript'>alert('ลบข้อมูลไม่สำเร็จ');window.history.go(-1);</script>";//javascript แจ้ง alert ข้อความ และคำสั่งให้ refresh หน้าเดิมถ้าลบข้อความไม่สำเร็จ
-}    
+  echo "Error deleting record: " . $conn->error;
+}
 
+$conn->close();
 ?>
