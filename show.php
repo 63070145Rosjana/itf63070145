d160aa009188a38dd3487b809cac1dd88ac5b77f
64070145Rosjana <73657654+64070145Rosjana@users.noreply.github.com>
2020-11-14 20:22:29 +0700

    Add files via upload

--- show.php
+++ show.php
@@ -36,7 +36,7 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook');
         <td><?php echo $Result['Name'];?></div></td>
         <td><?php echo $Result['Comment'];?></td>
         <td><?php echo $Result['Link'];?></td>
-        <td><?php echo $Result['Action'];?></td>
+        <a class="btn btn-warning" href="edit_r2.php?member_id=<?php echo $crow['member_id']; ?>">edit</a>
     </tr>
     <?php
     }
