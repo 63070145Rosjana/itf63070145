7d5aa729b14aac5c5eb6bdf7f734cce9f6ad6a2b
64070145Rosjana <73657654+64070145Rosjana@users.noreply.github.com>
2020-11-14 19:52:26 +0700

    Add files via upload

--- show.php
+++ show.php
@@ -18,27 +18,29 @@ if (mysqli_connect_errno($conn))
 }
 $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
-<table class="table table-bordered">
-  <tr>
-    <th width="100"> <div align="center">Name</div></th>
-    <th width="350"> <div align="center">Comment </div></th>
-    <th width="150"> <div align="center">Link </div></th>
-    <th width="150"> <div align="center">Action </div></th>
-  </tr>
-<?php
-while($Result = mysqli_fetch_array($res))
-{
-?>
-  <tr>
-    <td><?php echo $Result['Name'];?></div></td>
-    <td><?php echo $Result['Comment'];?></td>
-    <td><?php echo $Result['Link'];?></td>
-    <td><?php echo $Result['Action'];?></td>
-  </tr>
-<?php
-}
-?>
-</table>
+<div class="container">
+    <table class="table table-bordered">
+    <tr>
+        <th width="100"> <div align="center">Name</div></th>
+        <th width="350"> <div align="center">Comment </div></th>
+        <th width="150"> <div align="center">Link </div></th>
+        <th width="150"> <div align="center">Action </div></th>
+    </tr>
+    <?php
+    while($Result = mysqli_fetch_array($res))
+    {
+    ?>
+    <tr>
+        <td><?php echo $Result['Name'];?></div></td>
+        <td><?php echo $Result['Comment'];?></td>
+        <td><?php echo $Result['Link'];?></td>
+        <td><?php echo $Result['Action'];?></td>
+    </tr>
+    <?php
+    }
+    ?>
+    </table>
+</div>
 <?php
 mysqli_close($conn);
 ?>
