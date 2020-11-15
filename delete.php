f9476a10fc403624f4ec62dd0074ad2d8f0efa1b
64070145Rosjana <73657654+64070145Rosjana@users.noreply.github.com>
2020-11-15 15:13:24 +0700

    Add files via upload

--- delete.php
+++ delete.php
@@ -5,23 +5,16 @@ if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
-$member_id = $_REQUEST["Name"];
 // sql to delete a record
-$sql = "DELETE FROM guestbook WHERE member_id='$Name'";
-
-$result = mysqli_query($con, $sql) or die ("Error in query: $sql " . mysqli_error());
+$sql = "DELETE FROM guestbook WHERE Name='$result'";
  
 //จาวาสคริปแสดงข้อความเมื่อบันทึกเสร็จและกระโดดกลับไปหน้าฟอร์ม
 	
-	if($result){
-	echo "<script type='text/javascript'>";
-	echo "alert('delete Succesfuly');";
-	echo "window.location = 'ShowMember.php'; ";
-	echo "</script>";
-	}
-	else{
-	echo "<script type='text/javascript'>";
-	echo "alert('Error back to delete again');";
-	echo "</script>";
+if (mysqli_query($conn, $sql)) {
+  echo "Record deleted successfully";
+} else {
+  echo "Error deleting record: " . mysqli_error($conn);
 }
+
+mysqli_close($conn);
 ?>
