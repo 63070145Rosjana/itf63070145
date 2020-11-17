8ea159570efe676ffaf982829efa0fbb981fd41e
64070145Rosjana <73657654+64070145Rosjana@users.noreply.github.com>
2020-11-17 21:33:44 +0700

    Add files via upload

--- insert.php
+++ insert.php
@@ -1,3 +1,14 @@
+<html>
+<head>
+<title>delete</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
 <?php
 
 $conn = mysqli_init();
@@ -23,3 +34,8 @@ if (mysqli_query($conn, $sql)) {
   
 mysqli_close($conn);
 ?>
+<div class="panel-footer">
+  <tr><a href="show.php"><input type="submit" value="Home" class="btn btn-warning"></a></tr>
+</div>
+</body>
+</html>
