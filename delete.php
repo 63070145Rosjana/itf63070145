d3c27659e3f6dc1d427e9db8ad1c7d24446f6c90
64070145Rosjana <73657654+64070145Rosjana@users.noreply.github.com>
2020-11-17 20:31:15 +0700

    Add files via upload

--- delete.php
+++ delete.php
@@ -1,14 +1,3 @@
-<html>
-<head>
-<title>ITF Lab</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</head>
-<body>
 <?php
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'itf45.mysql.database.azure.com', 'rosjana@itf45', 'Pang0990062471', 'ITFLab', 3306);
@@ -33,5 +22,3 @@ if (mysqli_query($conn, $sql)) {
 mysqli_close($conn);
 ?>
 <tr><a href="show.php"><input type="submit" value="Home" class="btn btn-warning"></a></tr>
-</dody>
-</html>
