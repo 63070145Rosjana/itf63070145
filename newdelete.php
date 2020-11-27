0b65a7b8caf54304ff8f0a19531ccf04c995742a
64070145Rosjana <73657654+64070145Rosjana@users.noreply.github.com>
2020-11-27 12:39:20 +0700

    Add files via upload

--- newdelete.php
+++ newdelete.php
@@ -1,17 +1,41 @@
 <?php
-    $conn = mysqli_int();
-    mysqli_real_connect($conn, 'itf45.mysql.database.azure.com', 'rosjana@itf45', 'Pang0990062471', 'itflab', 3306);
-    if (mysqli_connect_errno($conn))
-{
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
-}
+    $conn = mysqli_connect($conn, 'itf45.mysql.database.azure.com', 'rosjana@itf45', 'Pang0990062471', 'itflab', 3306);
     $Id = $_GET['Id'];
-    $sql = "DELETE FROM gustbook2 WHERE Id = $Id";
-    if (mysqli_query($conn, $sql)) {
-        echo "Record deleted successfully";
-      } else {
-        echo "Error deleting record: " . mysqli_error($conn);
-    }
-      
-    mysqli_close($conn);
+	$sql = "DELETE FROM gustbook2 WHERE Id = $Id";
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Delete</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+	<style type="text/css">
+		.card {
+			border-top: solid 5px #37cf23;
+		}
+	</style>
+</head>
+<body class="bg-light py-5">
+	<div class="container text-dark">
+		<div class="row">
+			<div class="col-12 col-lg-8 offset-lg-2">
+				<div class="card shadow">
+					<div class="card-body">
+						<h2 align="center">
+						<?php
+							if(mysqli_query($conn, $sql)) {
+								echo "DELETE COMPLETED";
+							}
+							else {
+								echo "FAILED TO DELETE";
+							}
+						?>
+						</h2>
+						<p align="center" class="mt-4 mb-0"><a href="newindex.php" class="btn btn-sm btn-success">BACK</a></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
