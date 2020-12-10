8a73b6554d38fecfde17b82cc94255e75cd4d14e
64070145Rosjana <73657654+64070145Rosjana@users.noreply.github.com>
2020-12-10 23:19:17 +0700

    Add files via upload

---
+++ show_text.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+
+<body background="wallpaper.jpg">
+    <strong>Diary: </strong>
+    <?php
+        echo $_POST['name'];
+    ?>
+        <br>
+        <?php
+        echo $_POST['Date'];
+    ?>
+            <br>
+            <?php
+        echo $_POST['Diary'];
+    ?>
+                <br>
+                <?php
+        echo $_POST['Story'];
+    ?>
+</body>
+
+</html>
