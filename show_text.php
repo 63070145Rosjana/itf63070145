e2a7f09c6dbbdb11a1442508393488490c103737
64070145Rosjana <73657654+64070145Rosjana@users.noreply.github.com>
2020-12-11 00:00:24 +0700

    Update show_text.php

--- show_text.php
+++ show_text.php
@@ -3,25 +3,21 @@
 
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 
-<body background="wallpaper.jpg">
-    <strong>Diary: </strong>
-    <?php
-        echo $_POST['name'];
-    ?>
-        <br>
-        <?php
-        echo $_POST['Date'];
-    ?>
-            <br>
-            <?php
-        echo $_POST['Diary'];
-    ?>
-                <br>
-                <?php
-        echo $_POST['Story'];
-    ?>
+<body>
+    <div class="container">
+        <strong>Diary: </strong>
+        <td><?php echo $_POST['name'];?></td>
+        <td><?php echo $_POST['Date'];?></td>
+        <td><?php echo $_POST['Diary'];?></td>
+        <td><?php echo $_POST['Story'];?></td>
+    </div>
 </body>
 
 </html>
