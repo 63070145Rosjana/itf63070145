2f9512e93bfce725b68e8fdf1b8084fe1013c0df
64070145Rosjana <73657654+64070145Rosjana@users.noreply.github.com>
2020-12-13 15:20:14 +0700

    Update show_test.php

--- show_test.php
+++ show_test.php
@@ -67,7 +67,7 @@ body {
     <div class="paper">
         <div class="paper-content">
             <b style="font-size: 1.5rem;">Diary :<span><?php echo $_POST['Diary']?></span> by :<span><?php echo $_POST['name']?></span></b>
-            <b style="font-size: 0.5rem; text-align:right;" ><?php echo $_POST['Date']?></b>
+            <b style="text-align:right" ><?php echo $_POST['Date']?></b>
             <br>
             <span><?php echo $_POST['Story']?></span>
         </div>
